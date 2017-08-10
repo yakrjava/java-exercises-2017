f744061d28c1c9f76437e1510c21ee61aeee24e1
bahaluly <bahaluly@gmail.com>
2017-08-10 11:48:22 +0300

    first change yb

--- src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
+++ src/main/java/il/co/electriccollege/collections/CollectionsPlayground.java
@@ -11,7 +11,7 @@ public class CollectionsPlayground {
         System.out.println("Start list collection");
         ArrayList<String> stringList = new ArrayList<String>();
         stringList.add("hello aaa");
-        stringList.add("world");
+        stringList.add("world bbb");
 
         // the values of the list are accessible by index
         System.out.println("List member 0: " + stringList.get(0));
